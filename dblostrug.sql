1d8a4f459566fe6ac510d9c806601aaeaba9b52d
andrirahmanto <rahmanto660@gmail.com>
2020-12-17 02:04:05 +0700

    fix database

--- dblostrug.sql
+++ dblostrug.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Waktu pembuatan: 16 Des 2020 pada 07.23
+-- Waktu pembuatan: 16 Des 2020 pada 20.02
 -- Versi server: 10.4.13-MariaDB
 -- Versi PHP: 7.4.8
 
@@ -18,7 +18,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `logikalsupply`
+-- Database: `lostrug`
 --
 
 -- --------------------------------------------------------
@@ -54,74 +54,25 @@ INSERT INTO `admin` (`admin_id`, `admin_name`, `admin_email`, `admin_password`, 
 CREATE TABLE `order` (
   `order_id` int(11) NOT NULL,
   `user_id` int(11) NOT NULL,
+  `product_id` int(11) NOT NULL,
+  `order_key` varchar(50) DEFAULT NULL,
   `order_numberphone` varchar(50) NOT NULL,
   `order_address` varchar(100) NOT NULL,
+  `order_size` int(11) DEFAULT NULL,
+  `order_amount` int(11) NOT NULL,
+  `pesanan_ongkir` int(11) NOT NULL,
   `order_status` int(11) DEFAULT NULL,
   `order_payment` int(11) DEFAULT NULL,
-  `order_total_price` int(11) NOT NULL
+  `order_total_price` int(11) DEFAULT NULL,
+  `order_payment_method` varchar(50) DEFAULT NULL,
+  `order_payment_image` varchar(50) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data untuk tabel `order`
 --
 
-INSERT INTO `order` (`order_id`, `user_id`, `order_numberphone`, `order_address`, `order_status`, `order_payment`, `order_total_price`) VALUES
-(1, 1, '08127346512', 'jalan buaran no 6 jakarta timur kecamatan buaran', 0, 0, 450000),
-(2, 2, '081237654', 'jalan sumur batu jakarta pusat, kecamatan kemayoran', 0, 0, 465000);
-
--- --------------------------------------------------------
-
---
--- Struktur dari tabel `order_product`
---
-
-CREATE TABLE `order_product` (
-  `op_id` int(11) NOT NULL,
-  `order_id` int(11) NOT NULL,
-  `product_id` int(11) NOT NULL,
-  `op_size` int(50) NOT NULL,
-  `op_amount` int(50) NOT NULL,
-  `op_price` int(50) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- Dumping data untuk tabel `order_product`
---
-
-INSERT INTO `order_product` (`op_id`, `order_id`, `product_id`, `op_size`, `op_amount`, `op_price`) VALUES
-(1, 1, 1, 42, 1, 225000),
-(2, 1, 2, 42, 1, 225000),
-(3, 2, 2, 45, 1, 225000),
-(4, 2, 3, 45, 1, 240000);
-
--- --------------------------------------------------------
-
---
--- Struktur dari tabel `pesanan`
---
-
-CREATE TABLE `pesanan` (
-  `pesanan_id` int(11) NOT NULL,
-  `user_id` int(11) NOT NULL,
-  `product_id` int(11) NOT NULL,
-  `pesanan_key` varchar(50) DEFAULT NULL,
-  `pesanan_numberphone` varchar(50) NOT NULL,
-  `pesanan_address` varchar(100) NOT NULL,
-  `pesanan_size` int(11) DEFAULT NULL,
-  `pesanan_amount` int(11) NOT NULL,
-  `pesanan_ongkir` int(11) NOT NULL,
-  `pesanan_status` int(11) DEFAULT NULL,
-  `pesanan_payment` int(11) DEFAULT NULL,
-  `pesanan_total_price` int(11) DEFAULT NULL,
-  `pesanan_payment_method` varchar(50) DEFAULT NULL,
-  `pesanan_payment_image` varchar(50) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- Dumping data untuk tabel `pesanan`
---
-
-INSERT INTO `pesanan` (`pesanan_id`, `user_id`, `product_id`, `pesanan_key`, `pesanan_numberphone`, `pesanan_address`, `pesanan_size`, `pesanan_amount`, `pesanan_ongkir`, `pesanan_status`, `pesanan_payment`, `pesanan_total_price`, `pesanan_payment_method`, `pesanan_payment_image`) VALUES
+INSERT INTO `order` (`order_id`, `user_id`, `product_id`, `order_key`, `order_numberphone`, `order_address`, `order_size`, `order_amount`, `pesanan_ongkir`, `order_status`, `order_payment`, `order_total_price`, `order_payment_method`, `order_payment_image`) VALUES
 (30, 9, 1, '7103130307', '08127346512', 'jalan sumur batu jakarta pusat, kecamatan kemayoran', 43, 2, 20000, 4, 1, 469124, 'OVO', 'bukti.jpeg'),
 (31, 9, 1, '6131760626', '081234596254', 'Jl kramat sentiong Gg1', 40, 1, 20000, 1, 0, 244489, NULL, NULL);
 
@@ -134,8 +85,10 @@ INSERT INTO `pesanan` (`pesanan_id`, `user_id`, `product_id`, `pesanan_key`, `pe
 CREATE TABLE `product` (
   `product_id` int(11) NOT NULL,
   `product_name` varchar(50) NOT NULL,
+  `product_info` varchar(50) NOT NULL,
   `product_image` varchar(50) DEFAULT NULL,
   `product_description` varchar(100) DEFAULT NULL,
+  `product_stock` int(11) NOT NULL,
   `product_price` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
@@ -143,13 +96,10 @@ CREATE TABLE `product` (
 -- Dumping data untuk tabel `product`
 --
 
-INSERT INTO `product` (`product_id`, `product_name`, `product_image`, `product_description`, `product_price`) VALUES
-(1, 'Ventela Public Low black', 'black.png', NULL, 225000),
-(3, 'Ventela Public Low Dark Green', 'green.png', NULL, 240000),
-(4, 'Ventela Public Low Maroon', 'maroon.png', NULL, 245000),
-(6, 'Ventela Public Low Navy', 'navy.png', NULL, 240000),
-(7, 'Ventela BTS 70\'s High Natural Black', 'hi_black-natural.png', NULL, 245000),
-(8, 'Ventela BTS 70\'s High Dark Navy', 'hi_navy.png', NULL, 245000);
+INSERT INTO `product` (`product_id`, `product_name`, `product_info`, `product_image`, `product_description`, `product_stock`, `product_price`) VALUES
+(10, '\'You are under my control\'', 'T-shirt (black)', '1604991040283.jpg', NULL, 19, 189000),
+(11, '\'Miracle of Struggle\'', 'T-shirt (white)', '1604991085067.jpg', NULL, 23, 189000),
+(12, '\'Lost Struggle\'', 'T-shirt (black)', '1604991081160.jpg', NULL, 27, 189000);
 
 -- --------------------------------------------------------
 
@@ -216,18 +166,6 @@ ALTER TABLE `order`
   ADD PRIMARY KEY (`order_id`);
 
 --
--- Indeks untuk tabel `order_product`
---
-ALTER TABLE `order_product`
-  ADD PRIMARY KEY (`op_id`);
-
---
--- Indeks untuk tabel `pesanan`
---
-ALTER TABLE `pesanan`
-  ADD PRIMARY KEY (`pesanan_id`);
-
---
 -- Indeks untuk tabel `product`
 --
 ALTER TABLE `product`
@@ -259,25 +197,13 @@ ALTER TABLE `admin`
 -- AUTO_INCREMENT untuk tabel `order`
 --
 ALTER TABLE `order`
-  MODIFY `order_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
-
---
--- AUTO_INCREMENT untuk tabel `order_product`
---
-ALTER TABLE `order_product`
-  MODIFY `op_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
-
---
--- AUTO_INCREMENT untuk tabel `pesanan`
---
-ALTER TABLE `pesanan`
-  MODIFY `pesanan_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=32;
+  MODIFY `order_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=32;
 
 --
 -- AUTO_INCREMENT untuk tabel `product`
 --
 ALTER TABLE `product`
-  MODIFY `product_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+  MODIFY `product_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
 
 --
 -- AUTO_INCREMENT untuk tabel `status`
