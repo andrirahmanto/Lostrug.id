e55480d82a0c5e115a3e36c6428d4c3071eb2a45
andrirahmanto <rahmanto660@gmail.com>
2020-12-18 15:09:27 +0700

    update template admin

--- app/Controllers/Order.php
+++ app/Controllers/Order.php
@@ -6,6 +6,6 @@ class Order extends BaseController
 {
     public function index()
     {
-        echo view('admin/template.php');
+        echo view('admin/tableorder.php');
     }
 }
--- app/Views/admin/template.php
+++ app/Views/admin/template.php
@@ -75,69 +75,7 @@
         </div>
         <!-- end Data Admin -->
 
-        <!-- Tabel -->
-        <div class="card text-center">
-            <div class="card-header">
-                <ul class="nav nav-tabs card-header-tabs">
-                    <li class="nav-item">
-                        <a class="nav-link active" href="#">Order</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">Product</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">Account</a>
-                    </li>
-                </ul>
-            </div>
-            <div class="card-body">
-                <table id="tabel-orders" class="table table-bordered table-hover">
-                    <thead>
-                        <tr>
-                            <th>No</th>
-                            <th>Image</th>
-                            <th>Name</th>
-                            <th>Price</th>
-                            <th>Stock</th>
-                            <th>Actions</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <tr>
-                            <td>1</td>
-                            <td>
-                                <img class="img-fluid" style="width: 100px;" src="../assets/image/1604991081160.jpg" alt="">
-                            </td>
-                            <td>'Lost Struggle' T-shirt (balck)</td>
-                            <td>Rp 189.000</td>
-                            <td>12 pcs</td>
-                            <td>
-                                <div class="btn-group">
-                                    <a href="#" class="btn btn-primary btn-sm" data-toggle="modal" data-target="#modaleditproduct"><i class="fas fa-edit"></i></a>
-                                    <a href="#" class="btn btn-danger btn-sm" onclick="return confirm('Apakah Anda yakin ingin menghapus pesanan ini?')"><i class="fas fa-trash-alt"></i></a>
-                                </div>
-                            </td>
-                        </tr>
-                        <tr>
-                            <td>2</td>
-                            <td>
-                                <img class="img-fluid" style="width: 100px;" src="../assets/image/1604991081160.jpg" alt="">
-                            </td>
-                            <td>'Lost Struggle' T-shirt (balck)</td>
-                            <td>Rp 189.000</td>
-                            <td>12 pcs</td>
-                            <td>
-                                <div class="btn-group">
-                                    <a href="#" class="btn btn-primary btn-sm" data-toggle="modal" data-target="#modaleditproduct"><i class="fas fa-edit"></i></a>
-                                    <a href="#" class="btn btn-danger btn-sm" onclick="return confirm('Apakah Anda yakin ingin menghapus barang ini?')"><i class="fas fa-trash-alt"></i></a>
-                                </div>
-                            </td>
-                        </tr>
-                    </tbody>
-                </table>
-            </div>
-        </div>
-        <!-- End Table -->
+        <?= $this->renderSection('table'); ?>
 
     </div>
     <!-- end container -->
